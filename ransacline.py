679faba528c4a178add5087310286ecf5f5ef787
savannaos <savanna.smith@uconn.edu>
2017-01-31 12:12:33 -0500

    Savanna's ransac

--- ransacline.py
+++ ransacline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import randint
 
 #Generates noisy data points for a line
 #Using form y=mx+c
@@ -53,12 +54,56 @@ def CreateProblem():
 def RANSAC(x,y):
     solution_m=0
     solution_c=0
+    best_error=10000
+    data_size = len(x)
+    best_inliners = 0
+    thresh = 100
+    #for k number of iterations
+    k = 0
+    while(k<30):
+        inliner_error = 0
+        num_inliners = 0
+        #randomly select two pts from data
+        r1 = randint(0,data_size-1)
+        r2 = randint(0,data_size-1)
+        while r2 == r1: r2 = randint(0,len(x))
 
-    ##Your RANSAC solution here
-	##You may define additional functions as necessary
-	## Libraries are limited to the already added ones and the standard python ones
+        #fit line to pts
+        if(x[r1]>x[r2]):
+            curr_m,curr_c = fitline(x[r1],y[r1],x[r2],y[r2])
+        else:
+            curr_m,curr_c = fitline(x[r2],y[r2],x[r1],y[r1])
 
+        #print(x[r1],x[r2],y[r1],y[r2])
+        #for all data points not in curr_inliners
+        for i in range(0,2):
+            if i!= r1 and i!= r2:
+                pt_dist = testfit(x[i],y[i],curr_m,curr_c)
+                if pt_dist < thresh:
+                    #this point belongs to inliners
+                    inliner_error += pt_dist
+                    num_inliners += 1
+        if(inliner_error < best_error and num_inliners > best_inliners):
+            best_error = inliner_error
+            solution_m= curr_m
+            solution_c= curr_c
+        k+=1
     return solution_m,solution_c
+
+#takes in points a and b
+#outputs m and c for line equation
+def fitline(x1,y1,x2,y2):
+    m = (y2-y1)/(x2-x1)
+    c = y1-m*x1
+    return m,c
+
+#takes in point (x,y) and line parameters
+#outputs distance from point to line
+def testfit(x,y,m,c):
+    d1 = ((x+(m*y)-(m*c))/(m**2 +1))**2
+    d2 = (m*(x+(m*y)-(m*c))/(m**2 +1)+c-y)**2
+    d  = np.sqrt(d1+d2)
+    return d
 
 x,y=CreateProblem()
 
